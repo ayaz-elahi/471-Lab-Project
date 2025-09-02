6bf59a3f084852f518e73d594b6e140f208713f9
mdnaimrahman-cyber <md.naim.rahman@g.bracu.ac.bd>
2025-09-02 11:48:07 +0600

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-// backend/config/db.js
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.log("Error connecting to MongoDB:", err));
