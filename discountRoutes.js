4135feb461c4fcfc24734732e60cb5136cd53f70
mdnaimrahman-cyber <md.naim.rahman@g.bracu.ac.bd>
2025-09-02 11:47:57 +0600

    Delete discountRoutes.js

--- discountRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const discountController = require('../ controllers/discountController');
-
-// Discount routes
-router.get('/', discountController.getDiscounts);
-router.post('/apply', discountController.applyDiscount);
-
-module.exports = router;
